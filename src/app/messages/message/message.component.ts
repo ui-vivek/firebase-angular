467b9d6df043e10e605e6028c6184fbfb7fd33d1
js_devloper <meanjs.developer@gmail.com>
2025-02-12 14:46:25 +0530

    update message page

--- src/app/messages/message/message.component.ts
+++ src/app/messages/message/message.component.ts
@@ -1,20 +1,41 @@
 import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MessageDialogComponent } from '../message-dialog/message-dialog.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import {MatIconModule} from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CommonModule,
+    MatIconModule,
+    MatCardModule,
+    MatTableModule
+  ],
   selector: 'app-messages',
   templateUrl: './message.component.html',
-  styleUrls: ['./message.component.css'],
+  styleUrls: ['./message.component.scss'],
 })
 export class MessagesComponent {
+  messages: { email: string; message: string }[] = [];
+
   constructor(private dialog: MatDialog) {}
 
   openDialog() {
     const dialogRef = this.dialog.open(MessageDialogComponent);
     dialogRef.afterClosed().subscribe((result) => {
       if (result) {
-        console.log(result);
+        this.messages.push(result);
+        console.log(this.messages);
       }
     });
   }
